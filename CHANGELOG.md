49dbab02475b73e130d9fd65be3880b037c117f1
Stuart MacKay <smackay@flagstonesoftware.com>
2025-11-05 11:51:22 +0000

    New version: 0.4.0 → 0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Latest
 
+## 0.5.0 (2025-11-05)
+
 - Updated logging so messages describe the changes to the checklist. That
   makes them much more readable. 
 
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "eBird API Data"
 copyright = "2025, Stuart MacKay"
 author = "Stuart MacKay"
-release = "0.4.0"
+release = "0.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
